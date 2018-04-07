5fd767eb4ab90c3548405faf80df3b09edc03dd9
Dennis Tijbosch <d.tijbosch@yahoo.com>
2018-04-07 18:32:55 +0200

    list of pokemon's works

--- Pokedex/FirstViewController.swift
+++ Pokedex/FirstViewController.swift
@@ -8,12 +8,51 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tvPokemons: UITableView!
+    let list = ["Franje", "Sjors", "Piet", "Superfrans"]
+    var pokemonNames = [String]()
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (pokemonNames.count)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = pokemonNames[indexPath.row]
+        
+        return (cell!)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://pokeapi.co/api/v2/pokemon/")!
+        
+        let task = URLSession.shared.dataTask(with: url) {data, response, error in
+            do {
+                
+                let json = try JSONSerialization.jsonObject(with: data!) as! [String: Any]
+                let main: Array<AnyObject> = json["results"] as! Array<AnyObject>
+                for index in 0...main.count - 1 {
+                    let name = main[index]["name"] as! String
+                    self.pokemonNames.append(name)
+                }
+                
+                
+                
+                print(self.pokemonNames)
+                
+                DispatchQueue.main.async {
+                    self.tvPokemons.reloadData()
+                }
+            } catch {
+                
+            }
+        }
+        task.resume()
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
